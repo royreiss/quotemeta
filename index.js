a3c01795c569e24a972116e2e99bd9cc46dbd28a
Roy Reiss <rreiss@bitsnyc.com>
2016-07-13 13:57:55 -0400

    added .

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function (str) {
-    return String(str).replace(/(\[|\]|\{|\}|\+|\*|\?|\(|\)|\\|\$|\^)/g, '\\$1');
+    return String(str).replace(/(\[|\]|\{|\}|\+|\*|\?|\(|\)|\\|\$|\^|\.)/g, '\\$1');
 };
